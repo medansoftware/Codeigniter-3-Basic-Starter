e15ea6b755affaa817a6cb557cb2aa911dd00e81
agoenks29D <agungmasda29@gmail.com>
2022-01-09 18:38:01 +0700

    tidy up

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -1,6 +1,8 @@
 <?php (defined('BASEPATH')) OR exit('No direct script access allowed');
 
-/* load the MX_Router class */
-require APPPATH."third_party/HMVC/Router.php";
+// Load HMVC Router Class
+require (APPPATH.'third_party/HMVC/Router.php');
 
-class MY_Router extends HMVC_Router {}
+class MY_Router extends HMVC_Router
+{
+}
