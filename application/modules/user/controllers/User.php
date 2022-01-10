67735626090f1a94097356ba7b5c7f87cc381083
agoenks29D <agungmasda29@gmail.com>
2022-01-11 02:53:19 +0700

    changed view loader by template library

--- application/modules/user/controllers/User.php
+++ application/modules/user/controllers/User.php
@@ -14,12 +14,57 @@ class User extends HMVC_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->library('template');
+		$this->template->includes(array(
+			'header' => 'parts/header',
+			'navbar' => 'parts/navbar',
+			'sidebar_left' => 'parts/sidebar-left'
+		), array(
+			'header' => array(
+				'title' => 'Medan Software',
+				'meta' => array(
+					array('name' => 'description', 'content' => 'Codeigniter 3 Starter - Basic | Medan Software')
+				)
+			),
+			'sidebar_left' => array(
+				'brand' => array(
+					'name' => 'Medan Software',
+					'link' => site_url()
+				)
+			),
+			'navbar' => array(
+				'menus' => array(
+					array('link' => base_url($this->router->fetch_module()), 'name' => 'index', 'text' => 'Home'),
+					array('link' => base_url($this->router->fetch_module().'/about'), 'name' => 'about', 'text' => 'About')
+				)
+			),
+			'footer' => array(
+				'copyright' => 'Medan Software'
+			)
+		));
 	}
 
 	public function index()
 	{
-		$data['content'] = $this->load->view('home', array(), TRUE);
-		$this->load->view('base', $data);
+		$this->template->single_content('home',
+			array(
+				'include_vars' => array(
+					'header' => array(
+						'title' => 'User - Medan Software'
+					)
+				)
+			),
+			array(
+				// page vars
+				'page_info' => array(
+					'title' => 'Dashboard',
+					'breadcrumb' => array(
+						array('text' => 'Home', 'link' => base_url($this->router->fetch_module())),
+						array('text' => 'Dashboard', 'link' => base_url($this->router->fetch_module()))
+					)
+				)
+			)
+		);
 	}
 }
 
---
+++ application/modules/user/views/parts/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+	<strong>Copyright &copy; <?= date('Y') ?> <a href="https://adminlte.io"><?= $copyright ?></a>.</strong>
+	All rights reserved.
+	<div class="float-right d-none d-sm-inline-block">
+		<b>Version</b> <?= CI_VERSION ?>
+	</div>
+</footer>
