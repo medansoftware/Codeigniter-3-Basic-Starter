ca4d1b50f75f3b76e13bbf2419e7ecc520aa8f0c
agoenks29D <agungmasda29@gmail.com>
2022-01-11 01:45:19 +0700

    add sample of template library

--- application/modules/site/controllers/Site.php
+++ application/modules/site/controllers/Site.php
@@ -14,11 +14,66 @@ class Site extends HMVC_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->library('template');
+		$this->template->includes(array(
+			'header' => 'parts/header',
+			'navbar' => 'parts/navbar',
+			'footer' => 'parts/footer'
+		), array(
+			'header' => array(
+				'title' => 'Medan Software',
+				'meta' => array(
+					array('name' => 'description', 'content' => 'Codeigniter 3 Starter - Basic | Medan Software')
+				)
+			),
+			'navbar' => array(
+				'brand' => array(
+					'name' => 'Medan Software',
+					'link' => site_url()
+				),
+				'menus' => array(
+					array('link' => base_url($this->router->fetch_module()), 'name' => 'index', 'text' => 'Home'),
+					array('link' => base_url($this->router->fetch_module().'/about'), 'name' => 'about', 'text' => 'About')
+				)
+			),
+			'footer' => array(
+				'copyright' => 'Medan Software'
+			)
+		));
 	}
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->template->single_content('home');
+	}
+
+	public function about()
+	{
+		$this->template->single_content('about',
+			// base vars
+			array(
+				'include_vars' => array(
+					'header' => array(
+						'title' => 'About - Medan Software'
+					)
+				)
+			),
+			array(
+				// page vars
+				'text' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			),
+			//  method options
+			array(
+				'merge_vars' => TRUE,
+				'merge_vars_as_page' => TRUE,
+				'return_loaded' => FALSE
+			)
+		);
 	}
 }
 
